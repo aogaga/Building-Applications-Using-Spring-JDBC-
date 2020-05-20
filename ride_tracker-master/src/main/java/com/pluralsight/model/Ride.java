3fed2fe834ddf6400bdcc08636bdad45c01098dc
Ogheneogaga Fidelis Agi <aogaga@gmail.com>
2020-05-20 09:17:21 -0500

    checking so i can switch to intelli j

---
+++ ride_tracker-master/src/main/java/com/pluralsight/model/Ride.java
@@ -0,0 +1,33 @@
+package com.pluralsight.model;
+
+public class Ride {
+
+	private Integer Id;
+	private String name;
+	private int duration;
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
